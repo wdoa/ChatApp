9652c5a21e79f4dd5382166bc60b1cca78057206
13Griffon13 <13grifon13@gmail.com>
2015-11-01 21:31:17 +0200

    Frames
    немного формочек

---
+++ ChatApp/src/ChatFrame1.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by Griffon on 01.11.2015.
+ */
+public class ChatFrame1 {
+    private JTextField localIPTextField;
+    private JButton localConectButton;
+    private JTextField nickTextField;
+    private JButton conectButton;
+    private JTextField IPTextField;
+    private JTextField textField4;
+    private JButton sendButton;
+    private JTextArea textArea1;
+    private JButton showFriendListButton;
+}
---
+++ ChatApp/src/FriendList.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Griffon on 01.11.2015.
+ */
+public class FriendList {
+    private JList list1;
+    private JButton editButton;
+    private JButton delButton;
+    private JButton addButton;
+}
