7845c8964b6f8df9d124afcffdf63d4de68cb0dd
Anastasia Doroshkevich <auxerre00@gmail.com>
2015-11-01 21:39:48 +0200

    Revert "Revert "Frames""
    This reverts commit 7ec93ef733118a9107e4d08d8a0ab632d7680943.

---
+++ ChatApp/src/IncomingConection.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+/**
+ * Created by Griffon on 01.11.2015.
+ */
+public class IncomingConection {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
