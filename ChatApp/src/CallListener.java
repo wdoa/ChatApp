62f4dc20002a97847b2fedb3cc4b25cd96a8c371
wdoa <Liza@Lizka>
2015-11-04 20:13:24 +0200

    //

---
+++ ChatApp/src/CallListener.java
@@ -0,0 +1,72 @@
+import java.net.InetSocketAddress;
+
+import java.net.SocketAddress;
+import java.io.*;
+
+public class CallListener {
+	public String userName;
+	public Boolean statusBusy;
+	private final int localPort = 28411;
+	public SocketAddress remoteAddress;
+	public SocketAddress localAddress;
+    
+	public CallListener(String userName) {
+		this.userName = userName;
+		this.remoteAddress = new InetSocketAddress("127.0.0.0", localPort);
+	}
+    
+	public CallListener() {
+		userName ="name";
+	    remoteAddress = new InetSocketAddress("127.0.0.0", localPort);
+	}
+	Connection getConnection() throws IOException {
+		if (isStatusBusy()) {
+			return null;
+		} else {
+			
+			Connection connect = new Connection("localPort", userName);
+			return connect;
+		}
+
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public SocketAddress getLocalAddress() {
+		return localAddress;
+	}
+
+	public void setLocalAddress(SocketAddress localAddress) {
+		this.localAddress = localAddress;
+	}
+
+	public Boolean isStatusBusy() {
+		return statusBusy;
+	}
+
+	public void setStatus(Boolean statusBusy) {
+		this.statusBusy = statusBusy;
+	}
+
+	public SocketAddress getRemoteAddress() {
+		return remoteAddress;
+	}
+
+	public void setRemoteAddress(SocketAddress remoteAddress) {
+		this.remoteAddress = remoteAddress;
+	}
+
+	public int getLocalPort() {
+		return localPort;
+	}
+
+
+	
+
+}
