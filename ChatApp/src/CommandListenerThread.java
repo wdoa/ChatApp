2e5d3209858637aa37bfca263d8efe5467caeb97
Anastasia Doroshkevich <auxerre00@gmail.com>
2015-11-11 18:36:55 +0200

    com2

--- ChatApp/src/CommandListenerThread.java
+++ ChatApp/src/CommandListenerThread.java
@@ -1,4 +1,59 @@
+import java.io.IOException;
+import java.util.Observable;
 
-public class CommandListenerThread {
+public class CommandListenerThread extends Observable implements Runnable{
+	
+	private boolean isDisconnect;
+	private Connection connection;
+	private Command lastCommand;
 
+	
+	CommandListenerThread(Connection con) {
+		connection = con;
+	}
+
+	Command getLastCommand() {
+		return lastCommand;
+	}
+
+	boolean isDisconnected() {
+		return isDisconnect;
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	public void	run(){while (!isDisconnect) {
+				try {
+					lastCommand = connection.receive();		///?					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				}
+}
+ 
+	public void	start(){
+		isDisconnect= false;
+		run();
+		}  
+	
+	public void	stop() {
+		isDisconnect = true;	
+	}
+
+	public boolean isDisconnect() {
+		return isDisconnect;
+	}
+
+
+
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+
+	public void setLastCommand(Command lastCommand) {
+		this.lastCommand = lastCommand;
+	} 
+	
 }
